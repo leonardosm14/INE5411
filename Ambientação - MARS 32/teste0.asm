b1acbc7445e9df9b8308cb02357b190ab5a5c462
Leonardo de Sousa Marques <141475587+leonardosm14@users.noreply.github.com>
2024-09-01 15:51:25 -0300

    Primeiros Testes

--- Ambientação - MARS 32/teste0.asm
+++ Ambientação - MARS 32/teste0.asm
@@ -9,11 +9,11 @@
     la $a0, Hello
     syscall
     
-    # loading values of A and B;
+    # Carregando valores de A e B;
     lw $t0, ValorA      # Carrega o valor de ValorA em $t0
     lw $t1, ValorB      # Carrega o valor de ValorB em $t1
 
-    # performing the sum;
+    # Somando;
     add $t2, $t0, $t1   # Soma o valor de $t0 (ValorA) e $t1 (ValorB), resultado em $t2
 
     # printing A + B;
